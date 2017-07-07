c65458598b7550d559cba43456d6097651f90be3
bakhodir <bakhodir9293@gmail.com>
2017-07-07 19:39:34 +0500

    app.config file has been added

---
+++ src/app/app.config.ts
@@ -0,0 +1,5 @@
+export default{
+  server: 'http://192.168.1.105',
+  port: ':8088',
+  url: '/api'
+}
--- src/app/users/user.service.impl.ts
+++ src/app/users/user.service.impl.ts
@@ -3,12 +3,11 @@ import {User} from './user.model';
 import {BaseServiceImpl} from '../base/base.service.impl';
 import {Http} from '@angular/http';
 import {UserService} from './user.service';
+import * as  data from './../app.config';
 
 @Injectable()
 export class UserServiceImpl extends BaseServiceImpl<User> implements UserService {
-
   constructor(http: Http) {
-      super('http://192.168.1.105:8088/api/users', http);
+      super(data.default.server + data.default.port + data.default.url + '/users', http);
   }
-
 }
